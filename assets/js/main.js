dd284a0062b233814ed20a22f1ab4cac76a34c49
mariacheline <mariacheline@gmail.com>
2017-01-22 10:28:16 +0100

    Revert "Added a preloader"
    This reverts commit 5dab4f06b911867eedb887d08b1ad5f25cf9cf7f.
    
    # Conflicts:
    #	index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,14 +19,4 @@ $(document).ready(function(){
         });
         });
 
-        //Preloader
-        $(window).load(function() {
-        preloaderFadeOutTime = 500;
-        function hidePreloader() {
-        var preloader = $('.spinner-wrapper');
-        preloader.fadeOut(preloaderFadeOutTime);
-        }
-        hidePreloader();
-        });
-
 });
