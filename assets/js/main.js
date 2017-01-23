63438d004ef74de28878649bdaafe8ce2b14c3e8
mariacheline <mariacheline@gmail.com>
2017-01-23 11:32:01 +0100

    deleted window load

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,13 +20,10 @@ $(document).ready(function(){
         });
 
     //Preloader
-    $(window).load(function() {
     preloaderFadeOutTime = 500;
     function hidePreloader() {
     var preloader = $('.spinner-wrapper');
     preloader.fadeOut(preloaderFadeOutTime);
     }
     hidePreloader();
-  });
-
 });
